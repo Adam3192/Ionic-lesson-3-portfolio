e7e145ceab20eb5cf82d678da80ab0af919d3278
Adam3192 <jeremyadixon@hotmail.com>
2022-10-26 14:09:21 -0500

    Ionic Portfolio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Portfolio</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
