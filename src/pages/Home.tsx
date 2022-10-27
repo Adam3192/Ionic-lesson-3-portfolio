ae24d34ed6c1a304faf411be0cfda5f1b993dcff
Adam3192 <jeremyadixon@hotmail.com>
2022-10-27 13:29:01 -0500

    Ionic Portfolio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -31,21 +31,16 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <div id="container">
-          <IonGrid>
-            <IonRow>
-              <IonCol size-xs="4" size-md="2">
-                <IonThumbnail>
-                  <img alt="My Picture" src={picture} />
-                </IonThumbnail>
-              </IonCol>
-              <IonCol size-xs="8" size-md="10">
-                <div>
-                  <h1>Adam Dixon</h1>
-                  <p>Full Stack Developer</p>
-                </div>
-              </IonCol>
-            </IonRow>
-          </IonGrid>
+          <div className="flex padding2">
+            <IonThumbnail>
+              <img alt="My Picture" src={picture} />
+            </IonThumbnail>
+            <div className='padding'>
+              <h1>Adam Dixon</h1>
+              <p>Full Stack Developer</p>
+            </div>
+          </div>
+
           <div className="ion-padding">
             <h2 className="ion-text-start">About</h2>
             <p>
@@ -56,57 +51,68 @@ const Home: React.FC = () => {
               minus autem numquam.
             </p>
           </div>
-          <h2 className="ion-padding-start">Work Experience</h2>
-          <IonAccordionGroup>
-            <IonAccordion value="first">
-              <IonItem slot="header">
-                <IonLabel>Front End Developer, Google</IonLabel>
-              </IonItem>
-              <div className="ion-padding color" slot="content">
-                First Content
-              </div>
-            </IonAccordion>
-            <IonAccordion value="second">
-              <IonItem slot="header">
-                <IonLabel>Back End Developer, Amazon</IonLabel>
-              </IonItem>
-              <div className="ion-padding color" slot="content">
-                Second Content
-              </div>
-            </IonAccordion>
-            <IonAccordion value="third">
-              <IonItem slot="header">
-                <IonLabel>Cloud Engineer, Microsoft</IonLabel>
-              </IonItem>
-              <div className="ion-padding color" slot="content">
-                Third Content
-              </div>
-            </IonAccordion>
-          </IonAccordionGroup>
 
-          <h2 className="ion-padding-start">Technology Experience</h2>
-          <IonList>
-            <IonItem>
-              <IonLabel>Javascript</IonLabel>
-            </IonItem>
-            <IonItem>
-              <IonLabel>Typescript</IonLabel>
-            </IonItem>
-            <IonItem>
-              <IonLabel>React</IonLabel>
-            </IonItem>
-            <IonItem>
-              <IonLabel>MongoDB</IonLabel>
-            </IonItem>
-            <IonItem>
-              <IonLabel>Ionic</IonLabel>
-            </IonItem>
-          </IonList>
+          <IonGrid>
+            <IonRow>
+              <IonCol size-xs="12" size-md="6">
+                <h2 className="ion-padding-start">Work Experience</h2>
+                <IonAccordionGroup>
+                  <IonAccordion value="first">
+                    <IonItem slot="header">
+                      <IonLabel>Front End Developer, Google</IonLabel>
+                    </IonItem>
+                    <div className="ion-padding color" slot="content">
+                      First Content
+                    </div>
+                  </IonAccordion>
+                  <IonAccordion value="second">
+                    <IonItem slot="header">
+                      <IonLabel>Back End Developer, Amazon</IonLabel>
+                    </IonItem>
+                    <div className="ion-padding color" slot="content">
+                      Second Content
+                    </div>
+                  </IonAccordion>
+                  <IonAccordion value="third">
+                    <IonItem slot="header">
+                      <IonLabel>Cloud Engineer, Microsoft</IonLabel>
+                    </IonItem>
+                    <div className="ion-padding color" slot="content">
+                      Third Content
+                    </div>
+                  </IonAccordion>
+                </IonAccordionGroup>
+              </IonCol>
+
+              <IonCol size-xs="12" size-md="6">
+                <h2 className="ion-padding-start">Technology Experience</h2>
+                <IonList>
+                  <IonItem>
+                    <IonLabel>Javascript</IonLabel>
+                  </IonItem>
+                  <IonItem>
+                    <IonLabel>Typescript</IonLabel>
+                  </IonItem>
+                  <IonItem>
+                    <IonLabel>React</IonLabel>
+                  </IonItem>
+                  <IonItem>
+                    <IonLabel>MongoDB</IonLabel>
+                  </IonItem>
+                  <IonItem>
+                    <IonLabel>Ionic</IonLabel>
+                  </IonItem>
+                </IonList>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
         </div>
       </IonContent>
       <IonFooter>
         <IonToolbar color="primary">
-          <IonTitle className="size">jeremyadixon@hotmail.com</IonTitle>
+          <IonTitle className=" ion-text-center size">
+            jeremyadixon@hotmail.com
+          </IonTitle>
         </IonToolbar>
       </IonFooter>
     </IonPage>
