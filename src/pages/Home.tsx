b2468727743df0eae40e088610ae9466bf740540
Adam3192 <jeremyadixon@hotmail.com>
2022-10-27 11:46:52 -0500

    Ionic Portfolio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import {
+  IonAccordion,
+  IonAccordionGroup,
   IonCard,
   IonCol,
   IonContent,
+  IonFooter,
   IonGrid,
   IonHeader,
   IonIcon,
@@ -15,6 +18,8 @@ import {
   IonToolbar,
 } from '@ionic/react'
 import './Home.css'
+
+const picture = require('../Images/pic.png')
 
 const Home: React.FC = () => {
   return (
@@ -30,42 +35,80 @@ const Home: React.FC = () => {
             <IonRow>
               <IonCol size-xs="4" size-md="2">
                 <IonThumbnail>
-                  <img
-                    alt="My Picture"
-                    src="./Images/profilePic.png"
-                  />
+                  <img alt="My Picture" src={picture} />
                 </IonThumbnail>
               </IonCol>
-                <IonCol size-xs="8" size-md="10">
+              <IonCol size-xs="8" size-md="10">
                 <div>
-                <h1>Adam Dixon</h1>
-                <p>Full Stack Developer</p>
+                  <h1>Adam Dixon</h1>
+                  <p>Full Stack Developer</p>
                 </div>
-                </IonCol>
+              </IonCol>
             </IonRow>
           </IonGrid>
+          <div className="ion-padding">
+            <h2 className="ion-text-start">About</h2>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi
+              magnam et eum. Minus, consequatur illo. Provident iste repudiandae
+              explicabo amet ipsam, ducimus in facilis? Ipsum nemo quod neque,
+              eum eaque illum iure, corporis corrupti saepe blanditiis veritatis
+              minus autem numquam.
+            </p>
+          </div>
+          <h2 className="ion-padding-start">Work Experience</h2>
+          <IonAccordionGroup>
+            <IonAccordion value="first">
+              <IonItem slot="header">
+                <IonLabel>Front End Developer, Google</IonLabel>
+              </IonItem>
+              <div className="ion-padding color" slot="content">
+                First Content
+              </div>
+            </IonAccordion>
+            <IonAccordion value="second">
+              <IonItem slot="header">
+                <IonLabel>Back End Developer, Amazon</IonLabel>
+              </IonItem>
+              <div className="ion-padding color" slot="content">
+                Second Content
+              </div>
+            </IonAccordion>
+            <IonAccordion value="third">
+              <IonItem slot="header">
+                <IonLabel>Cloud Engineer, Microsoft</IonLabel>
+              </IonItem>
+              <div className="ion-padding color" slot="content">
+                Third Content
+              </div>
+            </IonAccordion>
+          </IonAccordionGroup>
 
-          <h2 className="ion-text-center">Contact Me</h2>
-
+          <h2 className="ion-padding-start">Technology Experience</h2>
           <IonList>
             <IonItem>
-              <IonLabel>contact@styledbylindy.com</IonLabel>
+              <IonLabel>Javascript</IonLabel>
             </IonItem>
             <IonItem>
-              <IonLabel>lindy-hoffman</IonLabel>
+              <IonLabel>Typescript</IonLabel>
             </IonItem>
             <IonItem>
-              <IonLabel>styledbylindy</IonLabel>
+              <IonLabel>React</IonLabel>
             </IonItem>
             <IonItem>
-              <IonLabel>@styledbylindy</IonLabel>
+              <IonLabel>MongoDB</IonLabel>
             </IonItem>
             <IonItem>
-              <IonLabel>lindyhoffman23</IonLabel>
+              <IonLabel>Ionic</IonLabel>
             </IonItem>
           </IonList>
         </div>
       </IonContent>
+      <IonFooter>
+        <IonToolbar color="primary">
+          <IonTitle className="size">jeremyadixon@hotmail.com</IonTitle>
+        </IonToolbar>
+      </IonFooter>
     </IonPage>
   )
 }
